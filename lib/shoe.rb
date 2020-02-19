c5ebc66870ddf2e119d95f8ee7e00e5c9db65561
Nicholas Patel <np4795@yahoo.com>
2020-02-19 20:08:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+
+  def cobble()
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+end
 # Make your shoe class here!
